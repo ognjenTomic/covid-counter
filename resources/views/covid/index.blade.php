5c21da5979234663528f2703d8081865459b230e
Olivier DEPIESSE <olivier.depiesse@gmail.com>
2020-04-06 11:54:05 +0200

    some CSS fixes

--- resources/views/covid/index.blade.php
+++ resources/views/covid/index.blade.php
@@ -13,7 +13,7 @@
     <body>
         <div class="flex-center position-ref full-height">
             <div class="content">
-                <div class="emoji m-b-md">
+                <div class="m-b-md">
                     <h1><span class="text-block blue">Lost</span><span class="text-block yellow">In</span><span class="text-block rose">Lockdown</span></h1>
                 </div>
                 <div class="emoji m-b-md">
