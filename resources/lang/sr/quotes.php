e0d24b6bb16f806dc2823314d474548feae071b9
Ognjen Tomic <53525853+ognjenTomic@users.noreply.github.com>
2020-04-14 17:25:07 +0200

    Update quotes.php

--- resources/lang/sr/quotes.php
+++ resources/lang/sr/quotes.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    '0' => 'We make a living by what we get, but we make a life by what we give.',
-    '1' => 'If you’re going through hell, keep going.',
-    '2' => 'You have enemies? Good. That means you’ve stood up for something, sometime in your life.',
-    '3' => 'To improve is to change, so to be perfect is to have changed often.',
-    '4' => 'Success consists of going from failure to failure without loss of enthusiasm.',
-    '5' => 'You will never get to the end of the journey if you stop to shy a stone at every dog that barks.',
-    '6' => 'Never give in, never give in, never, never, never, never—in nothing, great or small, large or petty—never give in except to convictions of honor and good sense.',
-    '7' => 'One always measures friendships by how they show up in bad weather.',
-    '8' => 'Success is not final, failure is not fatal: it is the courage to continue that counts.',
-    '9' => 'For myself I am an optimist — it does not seem to be much use being anything else.',
-    '10' => 'A pessimist sees the difficulty in every opportunity; an optimist sees the opportunity in every difficulty.',
+    '0' => 'Kroz život zarađujemo onim što dobijamo, ali život stvaramo onim što dajemo.',
+    '1' => 'Ako prolaziš kroz pakao, molim te nastavi i ne odustaj.',
+    '2' => 'Imaš neprijatelje? Dobro je. To znači da se zalažeš za nešto nekada u tvom životu.',
+    '3' => 'Poboljšati nešto, znači promena, da bi nešto bilo savršeno treba često menjati.',
+    '4' => 'Uspeh se sastoji od prelaska iz neuspeha u neuspeh bez gubitka entuzijazma.',
+    '5' => 'Nikada nećete stići do kraja putovanja ako prestanete da se stidite svakog psa koji laje.',
+    '6' => 'Nikada ne popustite, nikada ne popuštajte, nikada, nikada, nikada, nikada - ni u čemu, veliko ili malo, veliko ili sitno - nikada se ne predajte, osim u čast i dobar razum.',
+    '7' => 'Prijateljstvo se meri prema tome kako se prijatelj pojavljuju u lošem vremenu.',
+    '8' => 'Uspeh nije konačan, neuspeh nije fatalan: odvažnost je nastaviti.',
+    '9' => 'Za sebe sam optimista - izgleda da nije mnogo od koristi biti bilo šta drugo.',
+    '10' => 'Pesimista vidi poteškoću u svakoj prilici; optimista vidi priliku u svim poteškoćama.',
 ];
