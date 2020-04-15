a91c8cc65840ca1370c4642ff2f29bb6ac92b14d
Ognjen Tomic <53525853+ognjenTomic@users.noreply.github.com>
2020-04-15 12:21:56 +0200

    Update messages.php

--- resources/lang/sr/messages.php
+++ resources/lang/sr/messages.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'welcome' => '<p>Kod kuće si, ali nisi siguran/a koji je dan?</p><p>Ovde ćeš naći odgovoree :</p><p>Ti se nalaziš u <span class="text-block blue">:Country</span>, blizu <span class="text-block blue">:City</span> i danas je <span class="text-block yellow">:Today</span></p>',
+    'welcome' => '<p>Kod kuće si, ali nisi siguran/a koji je dan?</p><p>Ovde ćeš naći odgovore :</p><p>Ti se nalaziš u <span class="text-block blue">:Country</span>, blizu <span class="text-block blue">:City</span> i danas je <span class="text-block yellow">:Today</span></p>',
     'welcome-unknown' => '<p>Kod kuće si, ali nisi siguran/a koji je dan?</p><p>Here you\'ll find the answer :</p><p>It\'s <span class="text-block yellow">:Today</span></p>',
-    'title' => 'U samoizolaciji si od',
+    'title' => 'U samoizolaciji si već',
     'citation' => 'Ovde ide <span class="text-block rose">citat</span>  od Vinstona Čerčila :',
     'map' => 'map',
     'advices' => 'saveti',
