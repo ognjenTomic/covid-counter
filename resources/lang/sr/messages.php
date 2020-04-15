faa3087010dc94c2d6242b4d373eefc571066e25
Ognjen Tomic <53525853+ognjenTomic@users.noreply.github.com>
2020-04-15 11:52:39 +0200

    Update messages.php

--- resources/lang/sr/messages.php
+++ resources/lang/sr/messages.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'welcome' => '<p>Kod kuće si, ali nisi siguran/a koji je dan?</p><p>Ovde ćeš naći odgovoree :</p><p>Ti se nalaziš u <span class="text-block blue">:Country</span>, blizu <span class="text-block blue">:City</span> i danas je <span class="text-block yellow">:Today</span></p>',
+    'welcome' => '<p>Kod kuće si, ali nisi siguran/a koji je dan?</p><p>Ovde ćeš naći odgovore:</p><p>Ti se nalaziš u <span class="text-block blue">:Country</span>, blizu <span class="text-block blue">:City</span> i danas je <span class="text-block yellow">:Today</span></p>',
     'welcome-unknown' => '<p>Kod kuće si, ali nisi siguran/a koji je dan?</p><p>Here you\'ll find the answer :</p><p>It\'s <span class="text-block yellow">:Today</span></p>',
-    'title' => 'U samoizolaciji si od',
-    'citation' => 'Ovde ide <span class="text-block rose">citat</span>  od Vinstona Čerčila :',
-    'map' => 'map',
+    'title' => 'U samoizolaciji si već',
+    'citation' => 'Za vas <span class="text-block rose">citat</span>  od Vinstona Čerčila :',
+    'map' => 'mapa',
     'advices' => 'saveti',
     'toilet-paper' => 'https://www.amazon.com/s?k=toilet+paper',
 ];
