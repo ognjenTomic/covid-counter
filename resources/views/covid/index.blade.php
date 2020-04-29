d5339b8e28ad909b822be56d82172ac498309c99
Olivier DEPIESSE <olivier.depiesse@gmail.com>
2020-04-29 10:22:39 +0200

    trying to fix it

--- resources/views/covid/index.blade.php
+++ resources/views/covid/index.blade.php
@@ -71,20 +71,13 @@
                 @endif
                 <div class="container">
                     @if ($start != 'unknown')
-                    <div class="tick m-b-md"
-                        data-did-init="handleTickInit">
-                        <div data-repeat="true"
-                            data-layout="horizontal center fit"
-                            data-transform="preset(d, h, m, s) -> delay">
+                    <div class="tick m-b-md" data-did-init="handleTickInit">
+                        <div data-repeat="true" data-layout="horizontal center fit" data-transform="preset(d, h, m, s) -> delay">
                             <div class="tick-group">
-                                <div data-key="value"
-                                    data-repeat="true"
-                                    data-transform="pad(00) -> split -> delay">
+                                <div data-key="value" data-repeat="true" data-transform="pad(00) -> split -> delay">
                                     <span data-view="flip"></span>
                                 </div>
-                                <span data-key="label"
-                                    data-view="text"
-                                class="tick-label"></span>
+                                <span data-key="label" data-view="text" class="tick-label"></span>
                             </div>
                         </div>
                     </div>
